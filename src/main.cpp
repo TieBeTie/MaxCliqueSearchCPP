5827ee420da9fa62d4abeb228ea00ae3ebcdec19
Ramazan Kozhagulov <35148370+TieBeTie@users.noreply.github.com>
2024-03-23 08:58:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,11 @@ using std::cin;
 using std::cout;
 
 int main() {
-  int64_t n;
+  int n;
   cin >> n;
   vector<vector<bool>> graph(n, vector<bool>(n, false));
-  for (int64_t i = 0; i < n; ++i) {
-    for (int64_t j = 0; j < n; ++j) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
       bool is_edge;
       cin >> is_edge;
       graph[i][j] = is_edge;
